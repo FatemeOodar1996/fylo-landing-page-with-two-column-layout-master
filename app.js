dd25d7a33269208bef1e16c5d85f5e53b3788be0
Fateme Oodar <105479013+FatemeOodar1996@users.noreply.github.com>
2022-06-14 19:33:24 +0430

    edit_input_box

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var demo = document.getElementById("demo");
+var email = document.getElementById("email");
 function myFunction() {
   var at = document
     .getElementById("email")
@@ -6,6 +7,12 @@ function myFunction() {
   var fname = document.getElementById("email").value;
   submitOK = "true";
   if (at == -1) {
-    demo.innerHTML = "please write correct value";
+    demo.innerHTML = "please chek your email";
+    demo.style.color = "#dc3545";
+    email.style.borderColor = "#dc3545";
+  } else {
+    demo.innerHTML = "Your email has been registered";
+    email.style.borderColor = "#198754";
+    demo.style.color = "#198754";
   }
 }
